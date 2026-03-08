a44f06dbad2fd2b962c05eaa11dd32b700ad92df
Thrishanc <thrishanc4@gmail.com>
2026-03-08 13:17:32 +0530

    Updated teacher dashboard

--- teacher_app/smart_attendance_teacher/lib/screens/teacher_dashboard.dart
+++ teacher_app/smart_attendance_teacher/lib/screens/teacher_dashboard.dart
@@ -4,7 +4,6 @@ import 'package:flutter/services.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'view_attendance.dart';
-import 'package:fl_chart/fl_chart.dart';
 import '../services/ble_service.dart';
 import '../auth/teacher_login.dart';
 
